a1da2abf3784567d3ba1207371713e62d8dbbe0b
Samuel De Backer <sdebacker@gmail.com>
2014-11-19 23:31:50 +0100

    init

---
+++ src/TypiCMS/TranslatableBootForms/TranslatableBootFormsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace TypiCMS\TranslatableBootForms;
+
+use AdamWathan\BootForms\BootFormsServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class TranslatableBootFormsServiceProvider extends BootFormsServiceProvider {
+
+    protected function registerFormBuilder()
+    {
+        $this->app['adamwathan.form'] = $this->app->share(function($app)
+        {
+            $formBuilder = new TranslatableFormBuilder;
+            $formBuilder->setErrorStore($app['adamwathan.form.errorstore']);
+            $formBuilder->setOldInputProvider($app['adamwathan.form.oldinput']);
+            $formBuilder->setToken($app['session.store']->getToken());
+
+            return $formBuilder;
+        });
+    }
+
+}
---
+++ src/TypiCMS/TranslatableBootForms/TranslatableFormBuilder.php
@@ -0,0 +1,44 @@
+<?php
+namespace TypiCMS\TranslatableBootForms;
+
+use AdamWathan\Form\FormBuilder;
+use Config;
+
+class TranslatableFormBuilder extends FormBuilder
+{
+
+    public function bind($model)
+    {
+        $this->model = is_array($model) ? (object) $model : $model;
+    }
+
+    protected function getValueFor($name)
+    {
+        return parent::getValueFor($name);
+    }
+
+    protected function hasModelValue($name)
+    {
+        if (! isset($this->model)) {
+            return false;
+        }
+        return isset($this->model->{$name}) || method_exists($this->model, '__get');
+    }
+
+    /**
+     * Getting value from Model or ModelTranslation to populate form
+     * 
+     * @param  string $name key
+     * @return string       value
+     */
+    protected function getModelValue($name)
+    {
+        $array = preg_split('/[\[\]]+/', $name, -1, PREG_SPLIT_NO_EMPTY);
+        if (count($array) == 2 and in_array($array[0], Config::get('app.locales'))) {
+            list($lang, $name) = $array;
+            $value = $this->model->translate($lang)->{$name};
+            return $this->escapeQuotes($value);
+        }
+        return $this->escapeQuotes($this->model->{$name});
+    }
+}

f3554c8bb1711a4a5d8cc78e951f734156cfcd7a
Samuel De Backer <sdebacker@gmail.com>
2015-02-06 19:31:56 +0100

    get locales in config

--- src/TranslatableFormBuilder.php
+++ src/TranslatableFormBuilder.php
@@ -34,7 +34,7 @@ class TranslatableFormBuilder extends FormBuilder
     protected function getModelValue($name)
     {
         $array = preg_split('/[\[\]]+/', $name, -1, PREG_SPLIT_NO_EMPTY);
-        if (count($array) == 2 and in_array($array[0], Config::get('translatable::config.locales'))) {
+        if (count($array) == 2 and in_array($array[0], Config::get('translatable.locales'))) {
             list($lang, $name) = $array;
             $value = isset($this->model->translate($lang)->{$name}) ? $this->model->translate($lang)->{$name} : '' ;
             return $this->escapeQuotes($value);

e2c7c988cb85e27b55e7a2e4f0e9a9a396f53cc4
Samuel De Backer <sdebacker@gmail.com>
2015-10-17 11:25:13 -0400

    Applied fixes from StyleCI

--- src/TranslatableBootFormsServiceProvider.php
+++ src/TranslatableBootFormsServiceProvider.php
@@ -1,15 +1,15 @@
 <?php
+
 namespace TypiCMS\TranslatableBootForms;
 
 use AdamWathan\BootForms\BootFormsServiceProvider;
 
-class TranslatableBootFormsServiceProvider extends BootFormsServiceProvider {
-
+class TranslatableBootFormsServiceProvider extends BootFormsServiceProvider
+{
     protected function registerFormBuilder()
     {
-        $this->app['adamwathan.form'] = $this->app->share(function($app)
-        {
-            $formBuilder = new TranslatableFormBuilder;
+        $this->app['adamwathan.form'] = $this->app->share(function ($app) {
+            $formBuilder = new TranslatableFormBuilder();
             $formBuilder->setErrorStore($app['adamwathan.form.errorstore']);
             $formBuilder->setOldInputProvider($app['adamwathan.form.oldinput']);
             $formBuilder->setToken($app['session.store']->getToken());
@@ -17,5 +17,4 @@ class TranslatableBootFormsServiceProvider extends BootFormsServiceProvider {
             return $formBuilder;
         });
     }
-
 }
--- src/TranslatableFormBuilder.php
+++ src/TranslatableFormBuilder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace TypiCMS\TranslatableBootForms;
 
 use AdamWathan\Form\FormBuilder;
@@ -6,7 +7,6 @@ use Config;
 
 class TranslatableFormBuilder extends FormBuilder
 {
-
     public function bind($model)
     {
         $this->model = is_array($model) ? (object) $model : $model;
@@ -22,23 +22,27 @@ class TranslatableFormBuilder extends FormBuilder
         if (!isset($this->model)) {
             return false;
         }
+
         return isset($this->model->{$name}) || method_exists($this->model, '__get');
     }
 
     /**
-     * Getting value from Model or ModelTranslation to populate form
+     * Getting value from Model or ModelTranslation to populate form.
      *
-     * @param  string $name key
-     * @return string       value
+     * @param string $name key
+     *
+     * @return string value
      */
     protected function getModelValue($name)
     {
         $array = preg_split('/[\[\]]+/', $name, -1, PREG_SPLIT_NO_EMPTY);
         if (count($array) == 2 and in_array($array[0], Config::get('translatable.locales'))) {
             list($lang, $name) = $array;
-            $value = isset($this->model->translate($lang)->{$name}) ? $this->model->translate($lang)->{$name} : '' ;
+            $value = isset($this->model->translate($lang)->{$name}) ? $this->model->translate($lang)->{$name} : '';
+
             return $this->escape($value);
         }
+
         return $this->escape($this->model->{$name});
     }
 }
